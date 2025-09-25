234eb488e4413b0a6286692f95d8b5a3ddbc2539
Dionysis Lorentzos <ddl449@gmail.com>
2025-09-25 16:47:43 +0200

    Fix outputs and special chars

--- demos/android/MASVS-PLATFORM/MASTG-DEMO-xxx/run.sh
+++ demos/android/MASVS-PLATFORM/MASTG-DEMO-xxx/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-NO_COLOR=true semgrep -c ../../../../rules/mastg-android-sensitive-data-in-notifications.yml ./MastgTest_reversed.java --text -o output.txt
+NO_COLOR=true semgrep -c ../../../../rules/mastg-android-sensitive-data-in-notifications.yml ./MastgTest_reversed.java --text > output.txt
 
-NO_COLOR=true semgrep -c ../../../../rules/mastg-android-sensitive-data-in-notifications-manifest.yml ./AndroidManifest_reversed.xml --text -o output2.txt
+NO_COLOR=true semgrep -c ../../../../rules/mastg-android-sensitive-data-in-notifications-manifest.yml ./AndroidManifest_reversed.xml --text > output2.txt
